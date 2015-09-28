f607dea4da0af00557604073f0a2668edc0cf879
davidwintour <david.wintour@gmail.com>
2015-09-28 16:18:50 +0100

    Upload

--- index.php
+++ index.php
@@ -3,38 +3,18 @@
 <head>
     <title>foobar</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://octoraypfi.co.uk/css/body.css" type="text/css">
     <link rel="stylesheet" href="http://octoraypfi.co.uk/slicknav/dist/slicknav.css" type="text/css">
-    <script src="http://octoraypfi.co.uk/slicknav/dist/jquery.slicknav.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="http://octoraypfi.co.uk/slicknav/dist/jquery.slicknav.js"></script>
 
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 
     <script type="text/javascript">
     </script>
 
-    <script>
-        $('#menu').slicknav({
-            prependTo:'#demo1'
-        });
-    </script>
-
 </head>
 <body>
-
-<img src="http://octoraypfi.co.uk/images/banner.png" class="banner" alt="Header">
-
-<video width="400" controls autoplay >
-    <source src="http://octoraypfi.co.uk/video/des1.mp4" type="video/mp4">
-    Your browser does not support HTML5 video.
-</video>
-
 <p><b>Resize browser to view mobile menu</b></p>
 
 Original Menu hidden on mobile
@@ -63,11 +43,20 @@ Original Menu hidden on mobile
     </li>
 </ul>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('#menu').slicknav();
     });
 </script>
+<br>
+
+<img src="http://octoraypfi.co.uk/images/banner.png" class="banner" alt="Header">
+
+<video width="400" controls autoplay >
+    <source src="http://octoraypfi.co.uk/video/des1.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+</video>
 
 </body>
 </html>
