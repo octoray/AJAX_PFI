a6222b4c662fb0c8c28c1b6bce995b40d00a6840
davidwintour <david.wintour@gmail.com>
2015-09-28 12:41:37 +0100

    Upload

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <script type="text/javascript">
+    </script>
+
+</head>
+<body>
+
 <?php
 echo "hello";
 ?>
+
+
+</body>
+</html>
