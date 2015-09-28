a95a1cc0ee0ebb73b928f1ac1df78d1e8af7d044
davidwintour <david.wintour@gmail.com>
2015-09-28 16:14:31 +0100

    Upload

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="http://octoraypfi.co.uk/css/body.css" type="text/css">
     <link rel="stylesheet" href="http://octoraypfi.co.uk/slicknav/dist/slicknav.css" type="text/css">
     <script src="http://octoraypfi.co.uk/slicknav/dist/jquery.slicknav.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 
 
     <script type="text/javascript">
@@ -25,15 +27,6 @@
 
 </head>
 <body>
-<ul id="menu">
-    <li><a class="scroll" href="#features">Features</a></li>
-    <li><a class="scroll" href="#usage">Usage Instructions</a></li>
-    <li><a class="scroll" href="#examples">Examples</a></li>
-    <li><a href="http://github.com">View on Github</a></li>
-</ul>
-
-<div id="demo1">
-    </div>
 
 <img src="http://octoraypfi.co.uk/images/banner.png" class="banner" alt="Header">
 
@@ -41,6 +34,40 @@
     <source src="http://octoraypfi.co.uk/video/des1.mp4" type="video/mp4">
     Your browser does not support HTML5 video.
 </video>
+
+<p><b>Resize browser to view mobile menu</b></p>
+
+Original Menu hidden on mobile
+<ul id="menu">
+    <li>Parent 1
+        <ul>
+            <li><a href="#">item 3</a></li>
+            <li>Parent 3
+                <ul>
+                    <li><a href="#">item 8</a></li>
+                    <li><a href="#">item 9</a></li>
+                    <li><a href="#">item 10</a></li>
+                </ul>
+            </li>
+            <li><a href="#">item 4</a></li>
+        </ul>
+    </li>
+    <li><a href="#">item 1</a></li>
+    <li>non-link item</li>
+    <li>Parent 2
+        <ul>
+            <li><a href="#">item 5</a></li>
+            <li><a href="#">item 6</a></li>
+            <li><a href="#">item 7</a></li>
+        </ul>
+    </li>
+</ul>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#menu').slicknav();
+    });
+</script>
 
 </body>
 </html>
