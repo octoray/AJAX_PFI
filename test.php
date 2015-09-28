aefb5da5f02360ba5317dfbe0e96761088136bb9
davidwintour <david.wintour@gmail.com>
2015-09-28 16:38:33 +0100

    Upload

--- test.php
+++ test.php
@@ -13,21 +13,12 @@
 </head>
 
 <ul id="menu">
-    <li>Parent 1
+    <li>Offers
         <ul>
-            <li><a href="#">item 3</a></li>
-            <li>Parent 3
-                <ul>
-                    <li><a href="#">item 8</a></li>
-                    <li><a href="#">item 9</a></li>
-                    <li><a href="#">item 10</a></li>
-                </ul>
-            </li>
+            <li><a href="#">item1 </a></li>
             <li><a href="#">item 4</a></li>
         </ul>
     </li>
-    <li><a href="#">item 1</a></li>
-    <li>non-link item</li>
     <li>Parent 2
         <ul>
             <li><a href="#">item 5</a></li>
