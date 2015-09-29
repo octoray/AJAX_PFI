83fd4f1e03b592b451d7e037f192c136bbc33c1f
davidwintour <david.wintour@gmail.com>
2015-09-29 10:50:49 +0100

    Upload

---
+++ test2.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width">
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/slicknav/dist/slicknav.css">
+    <link rel="stylesheet" href="http://octoraypfi.co.uk/css/body.css" type="text/css">
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+</head>
+
+<ul id="menu">
+    <li>Offers
+        <ul>
+            <li><a href="#">EXPANSION I Offer</a></li>
+            <li><a href="#">EXPANSION II Offer</a></li>
+        </ul>
+    </li>
+    <li>The Taken King Offer
+        <ul>
+            <li><a href="#">Abilitys</a></li>
+            <li><a href="#">New Gear</a></li>
+            <li><a href="#">New Subclasses</a></li>
+        </ul>
+    </li>
+</ul>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="/slicknav/dist/jquery.slicknav.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#menu').slicknav();
+    });
+</script>
+
+<img src="http://octoraypfi.co.uk/images/banner.png" class="banner" alt="Header">
+
+<div class="block_1">
+<div class="btn1">
+Buy Expansion Pack For £30 (Charged To Your Mobile)
+</div>
+    <br>
+    <div class="btn1">
+        Buy Expansion Pack For £30 (Charged To Your Mobile)
+    </div>
+</div>
+
+
+<div class="footer">
+    <br>
+    <img src="https://www.destinythegame.com/content/dam/atvi/global/firstparty/activision/activision-logo.svg"  class="foot_logo" alt="Header"><br><br>
+A minimum of 60 GB available hard drive storage space required as of September 2015 (PS4 and Xbox One). A minimum of 30 GB available hard drive storage space required as of September 2015 (PS3 and Xbox 360). Storage space requirements are subject to change and may increase. If you are purchasing after September 2015, please visit www.destinythegame.com/size-requirements for current storage requirements prior to purchase. Additional storage may be required for setup, some updates and features. There may be mandatory in-game updates which users must download to continue playing and which may require additional storage. Storage requirements subject to change. Broadband internet required. Users are responsible for broadband access and usage fees. Additional charges can apply for online content and some features. Various elements and functionality of PS4 Destiny Game require subscription to PlayStation Plus, sold separately. Various elements and functionality of Xbox One and Xbox 360 Destiny Game require subscription to Xbox Live, sold separately. Please see www.destinythegame.com for further details. Terms and conditions apply.
+</div>
+
+</body>
+</html>
