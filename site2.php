442b990026765ad6858886aa9fa706e24e5d0cf2
davidwintour <david.wintour@gmail.com>
2015-09-30 16:27:03 +0100

    Upload

--- site2.php
+++ site2.php
@@ -42,77 +42,8 @@
 </script>
 <div class="block_1">
 <img src="http://octoraypfi.co.uk/images/logo.png" class="banner" alt="Header">
+    </div>
 <br>
-    <br><br><br><br><br><br><br>
-<div class="btn1">
-Buy Expansion Pack For £30 <br>(Charged To Your Mobile)
-</div>
-    <br>
-    <div class="btn1">
-        Learn More ...
-    </div>
-</div>
-
-<div class="block_66">
-    <br>
-    <img src="https://www.destinythegame.com/content/dam/atvi/bungie/dtg-comet/utils/logos/destiny-taken-king-composite-logo.svg" class="banner" alt="Header"> <br>
-    <h2>Available Now</h2>
-    <br>
-</div>
-
-<div class="block_2">
-<video class="video" width="100%"  poster="http://octoraypfi.co.uk/images/image1.jpg" autoplay="autoplay" controls >
-    <source src="http://octoraypfi.co.uk/video/des1.mp4" type="video/mp4">
-    Your browser does not support HTML5 video.
-</video>
-</div>
-
-<div class="block_6">
-    <br>
-    <h1>The Taken King Live Action Trailer – Evil’s Most Wanted</h1> <br>
-    You killed his son. Now Oryx, The Taken King, wants revenge. Gather your Fireteam and take on the greatest threat this universe has seen with Destiny: The Taken King.
-    <br><br><br>
-</div>
-
-<div class="block_4">
-    <img src="http://octoraypfi.co.uk/images/box1.jpg" class="image2" alt="Header" width="100%">
-</div>
-
-
-<div class="block_6">
-    <br>
-    <h1>The Taken King - Legendary Edition</h1> <br>
-    Destiny: The Taken King - Legendary Edition is perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legends in the stars.
-    <br><br><br>
-</div>
-
-<div class="block_4">
-    <img src="http://octoraypfi.co.uk/images/image2.png" class="image2" alt="Header" width="100%">
-</div>
-
-<div class="block_6"><br>
-    <h1>THIS IS YOUR LEGEND</h1> <br>
-    ACCESS YOUR ARSENAL, TRACK YOUR STATS, AND TEAM UP WITH OTHER GUARDIANS.
-    <br><br><br>
-</div>
-
-<div class="block_5">
-    <img src="http://octoraypfi.co.uk/images/gif.gif" class="image2" alt="Header" width="100%">
-</div>
-
-
-<div class="block_6">
-    <h1 class="edition-title">
-        The Taken King - <br>Legendary Edition
-    </h1><br>
-    Destiny: &nbsp;The Taken King -&nbsp;Legendary Edition is&nbsp;perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legend in the stars. &nbsp;The Legendary Edition features the complete Destiny experience including The Taken King, the next evolution in the Destiny universe, Destiny, Expansion I: The Dark Below and Expansion II: House of Wolves. &nbsp;Unite with 20 million Guardians in the fight to defeat Earth’s enemies, reclaim all that we have lost, and Become Legend.
-<br><br><br><br>
-    <div class="btn1">
-        Buy Expansion Pack For £30 <br> (Charged To Your Mobile)
-    </div>
-    <br><br><br>
-</div>
-
 
 
 <div class="footer">
