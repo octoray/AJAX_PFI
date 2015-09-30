94c254b443d1320a987b2cb0cce781db28119411
davidwintour <david.wintour@gmail.com>
2015-09-30 16:23:02 +0100

    Upload

---
+++ site2.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width">
+    <meta charset="UTF-8">
+    <script type="text/javascript">
+    $(function(){
+    $("body").hide();
+    $("body").fadeIn(1000);
+    })
+    </script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/slicknav/dist/slicknav.css">
+    <link rel="stylesheet" href="http://octoraypfi.co.uk/css/body2.css" type="text/css">
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+</head>
+
+<ul id="menu">
+    <li>Offers
+        <ul>
+            <li><a href="#">EXPANSION I Offer</a></li>
+            <li><a href="#">EXPANSION II Offer</a></li>
+        </ul>
+    </li>
+    <li>The Taken King Offer
+        <ul>
+            <li><a href="#">Abilitys</a></li>
+            <li><a href="#">New Gear</a></li>
+            <li><a href="#">New Subclasses</a></li>
+        </ul>
+    </li>
+</ul>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="/slicknav/dist/jquery.slicknav.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#menu').slicknav();
+    });
+</script>
+<div class="block_1">
+<img src="http://octoraypfi.co.uk/images/logo.png" class="banner" alt="Header">
+<br>
+    <br><br><br><br><br><br><br>
+<div class="btn1">
+Buy Expansion Pack For £30 <br>(Charged To Your Mobile)
+</div>
+    <br>
+    <div class="btn1">
+        Learn More ...
+    </div>
+</div>
+
+<div class="block_66">
+    <br>
+    <img src="https://www.destinythegame.com/content/dam/atvi/bungie/dtg-comet/utils/logos/destiny-taken-king-composite-logo.svg" class="banner" alt="Header"> <br>
+    <h2>Available Now</h2>
+    <br>
+</div>
+
+<div class="block_2">
+<video class="video" width="100%"  poster="http://octoraypfi.co.uk/images/image1.jpg" autoplay="autoplay" controls >
+    <source src="http://octoraypfi.co.uk/video/des1.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+</video>
+</div>
+
+<div class="block_6">
+    <br>
+    <h1>The Taken King Live Action Trailer – Evil’s Most Wanted</h1> <br>
+    You killed his son. Now Oryx, The Taken King, wants revenge. Gather your Fireteam and take on the greatest threat this universe has seen with Destiny: The Taken King.
+    <br><br><br>
+</div>
+
+<div class="block_4">
+    <img src="http://octoraypfi.co.uk/images/box1.jpg" class="image2" alt="Header" width="100%">
+</div>
+
+
+<div class="block_6">
+    <br>
+    <h1>The Taken King - Legendary Edition</h1> <br>
+    Destiny: The Taken King - Legendary Edition is perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legends in the stars.
+    <br><br><br>
+</div>
+
+<div class="block_4">
+    <img src="http://octoraypfi.co.uk/images/image2.png" class="image2" alt="Header" width="100%">
+</div>
+
+<div class="block_6"><br>
+    <h1>THIS IS YOUR LEGEND</h1> <br>
+    ACCESS YOUR ARSENAL, TRACK YOUR STATS, AND TEAM UP WITH OTHER GUARDIANS.
+    <br><br><br>
+</div>
+
+<div class="block_5">
+    <img src="http://octoraypfi.co.uk/images/gif.gif" class="image2" alt="Header" width="100%">
+</div>
+
+
+<div class="block_6">
+    <h1 class="edition-title">
+        The Taken King - <br>Legendary Edition
+    </h1><br>
+    Destiny: &nbsp;The Taken King -&nbsp;Legendary Edition is&nbsp;perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legend in the stars. &nbsp;The Legendary Edition features the complete Destiny experience including The Taken King, the next evolution in the Destiny universe, Destiny, Expansion I: The Dark Below and Expansion II: House of Wolves. &nbsp;Unite with 20 million Guardians in the fight to defeat Earth’s enemies, reclaim all that we have lost, and Become Legend.
+<br><br><br><br>
+    <div class="btn1">
+        Buy Expansion Pack For £30 <br> (Charged To Your Mobile)
+    </div>
+    <br><br><br>
+</div>
+
+
+
+<div class="footer">
+    <br>
+    <img src="https://www.destinythegame.com/content/dam/atvi/global/firstparty/activision/activision-logo.svg"  class="foot_logo" alt="Header"><br><br>
+A minimum of 60 GB available hard drive storage space required as of September 2015 (PS4 and Xbox One). A minimum of 30 GB available hard drive storage space required as of September 2015 (PS3 and Xbox 360). Storage space requirements are subject to change and may increase. If you are purchasing after September 2015, please visit www.destinythegame.com/size-requirements for current storage requirements prior to purchase. Additional storage may be required for setup, some updates and features. There may be mandatory in-game updates which users must download to continue playing and which may require additional storage. Storage requirements subject to change. Broadband internet required. Users are responsible for broadband access and usage fees. Additional charges can apply for online content and some features. Various elements and functionality of PS4 Destiny Game require subscription to PlayStation Plus, sold separately. Various elements and functionality of Xbox One and Xbox 360 Destiny Game require subscription to Xbox Live, sold separately. Please see www.destinythegame.com for further details. Terms and conditions apply.
+</div>
+
+</body>
+</html>
