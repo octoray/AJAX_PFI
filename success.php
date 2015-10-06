b598b3e467df2326e40af6a7c267e9076914ee88
davidwintour <david.wintour@gmail.com>
2015-10-06 12:00:57 +0100

    Upload

--- success.php
+++ success.php
@@ -6,20 +6,19 @@ function getGUID(){
         mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
         $charid = strtoupper(md5(uniqid(rand(), true)));
         $hyphen = chr(45);// "-"
-        $uuid = chr(123)// "{"
-            .substr($charid, 0, 8).$hyphen
+        $uuid = // "{"
+            substr($charid, 0, 8).$hyphen
             .substr($charid, 8, 4).$hyphen
             .substr($charid,12, 4).$hyphen
             .substr($charid,16, 4).$hyphen
-            .substr($charid,20,12)
-            .chr(125);// "}"
+            .substr($charid,20,12);
         return $uuid;
     }
 }
 
 $GUID = getGUID();
 //echo $GUID;
-header("bob: ".$GUID);
+header("X-PFI-SessionToken: ".$GUID);
 header('X-PFI-MerchantToken: 57D92441-6B7F-4691-936E-10836CB92496');
 ?>
 <!DOCTYPE HTML>
