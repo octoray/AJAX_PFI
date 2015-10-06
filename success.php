59f64e60642c4e40f9c031df4a3e955d233ff42f
davidwintour <david.wintour@gmail.com>
2015-10-06 12:29:40 +0100

    Upload

--- success.php
+++ success.php
@@ -17,8 +17,13 @@ function getGUID(){
 }
 
 $GUID = getGUID();
-header("X-PFI-SessionToken: ".$GUID);
-header('X-PFI-MerchantToken: 57D92441-6B7F-4691-936E-10836CB92496');
+if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
+    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
+    header('X-PFI-MerchantToken: 57D92441-6B7F-4691-936E-10836CB92496');
+}else{
+    header("X-PFI-SessionToken: ".$GUID);
+    header('X-PFI-MerchantToken: 57D92441-6B7F-4691-936E-10836CB92496');
+};
 ?>
 <!DOCTYPE HTML>
 <html>
