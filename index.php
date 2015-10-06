3c96220f477c4a3c8e10e66a0bb17e9c00cc66c0
davidwintour <david.wintour@gmail.com>
2015-10-06 13:18:56 +0100

    Upload

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
+<?php
+function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = // "{"
+            substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12);
+        return $uuid;
+    }
+}
 
+$GUID = getGUID();
+if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
+    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
+    header('X-PFI-MerchantToken: 57D92441-6B7F-4691-936E-10836CB92496');
+}else{
+    header("X-PFI-SessionToken: ".$GUID);
+    header('X-PFI-MerchantToken: 57D92441-6B7F-4691-936E-10836CB92496');
+};
+?>
 <!DOCTYPE html>
 <html>
 <head>
