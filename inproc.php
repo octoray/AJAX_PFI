ed749a5877c95c2a0691c2affac6008524cde1a3
davidwintour <david.wintour@gmail.com>
2015-10-08 14:56:08 +0100

    Upload

--- inproc.php
+++ inproc.php
@@ -81,12 +81,3 @@ if ($_SERVER['HTTP_X_PFI_STATUS'] == "PfiProcessing") {
 };
 
 ?>
-
-
-if ($a > $b) {
-echo "a is bigger than b";
-} elseif ($a == $b) {
-echo "a is equal to b";
-} else {
-echo "a is smaller than b";
-}
