8c5cc0a054b062344e1b974230572f7b37f6925d
davidwintour <david.wintour@gmail.com>
2015-10-13 09:54:38 +0100

    Upload

--- inproc.php
+++ inproc.php
@@ -27,33 +27,6 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
 };
 
 
-//Write logs
-$today1 = date("Y-m-d H:i:s");
-$myFile = "log.txt";
-$fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = "\n";
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": GUID Set?: " .$set);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": Sessiontoken: " .$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": status: " .$_SERVER['HTTP_X_PFI_STATUS']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": requestime: " .$_SERVER['HTTP_X_PFI_REQUESTTIME']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": hash: " .$_SERVER['HTTP_X_PFI_HASH']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": alias: " .$_SERVER['HTTP_X_PFI_ALIAS']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": netinfo: " .$_SERVER['HTTP_X_PFI_NETINFO']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": callerid / function: " .$_SERVER['HTTP_X_PFI_CALLERID']);
-fwrite($fh, $stringData);
-fwrite($fh, $today1. ": response was: ".$response);
-fwrite($fh, $stringData);
-fclose($fh);
-
-
 $response1 = '{
 	"Error":null,
 	"Script":"hidebutton()",
@@ -85,11 +58,40 @@ if ($_SERVER['HTTP_X_PFI_STATUS'] == "PfiProcessing") {
     $response = $response3;
     echo $response3;
 }elseif ($_SERVER['HTTP_X_PFI_STATUS'] == "PfiPurchaseSuccessa"){
-       $var = '1';
+    $var = '1';
 }else{
     $response = $response1;
     echo $response1;
 };
+
+
+//Write logs
+$today1 = date("Y-m-d H:i:s");
+$myFile = "log.txt";
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = "\n";
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": GUID Set?: " .$set);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": Sessiontoken: " .$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": status: " .$_SERVER['HTTP_X_PFI_STATUS']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": requestime: " .$_SERVER['HTTP_X_PFI_REQUESTTIME']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": hash: " .$_SERVER['HTTP_X_PFI_HASH']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": alias: " .$_SERVER['HTTP_X_PFI_ALIAS']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": netinfo: " .$_SERVER['HTTP_X_PFI_NETINFO']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": callerid / function: " .$_SERVER['HTTP_X_PFI_CALLERID']);
+fwrite($fh, $stringData);
+fwrite($fh, $today1. ": response was: ".$response);
+fwrite($fh, $stringData);
+fclose($fh);
+
+
 
 
 
