6533775f4208ee8a6a99d7329d39ed4defeefd42
davidwintour <david.wintour@gmail.com>
2015-10-21 17:02:34 +0100

    Upload

--- testPB.php
+++ testPB.php
@@ -1,10 +1,56 @@
-<!doctype html>
+<?php
+function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);
+        $uuid =
+            substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12);
+        return $uuid;
+    }
+}
 
+$GUID = getGUID();
+if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
+    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
+    $set = 'set';
+}else{
+    header("X-PFI-SessionToken: ".$GUID);
+    $notset = 'not set';
+};
+?>
+<!DOCTYPE html>
+<html>
 <head>
-    <meta name="format-detection" content="telephone=no"/>
-    <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
-    <meta charset="UTF-8"/>
+    <title>Main</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="http://octoraypfi.co.uk/css/body.css" type="text/css">
+    <link rel="stylesheet" href="http://octoraypfi.co.uk/slicknav/dist/slicknav.css" type="text/css">
+    <script src="http://octoraypfi.co.uk/slicknav/dist/jquery.slicknav.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(function(){
+            $("body").fadeOut(4200,function(){
+                window.location.href = "http://the-sloth.uk/test2.php"
+            })
+        });
+    </script>
+
 </head>
+<body>
 
 
-HELLO
+<img src="http://octoraypfi.co.uk/images/banner.png" class="banner_main" alt="Header">
+
+
+</body>
+</html>
