eea38621c88c9cb2b09556563b339aaeb3ee911d
davidwintour <david.wintour@gmail.com>
2015-10-22 15:00:40 +0100

    Upload

--- testPB.php
+++ testPB.php
@@ -6,27 +6,29 @@
     <meta name="viewport" content="width=device-width">
     <meta charset="UTF-8">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+    <!-- Scripts
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+    -->
 
-</head>
-<body>
+    </head>
+    <body>
 
-<!-- 1st block /Purchase button -->
+    <!-- 1st block / Purchase button -->
 <div class="merchant-button btn1" id="content_1"
      data-pfi-button="true"
-     data-pfi-button-id="000001"
+     data-pfi-button-id="this_1"
      data-pfi-service-id="330"
      data-pfi-reference="987654321"
      data-pfi-content-url="/success.php"
      data-pfi-content-id="987654321">
-    <p>Buy For £5</p>
+    <button>Buy For £5</button>
 </div>
 
 <br>
 
 <!-- 2nd block load stuff  -->
-<div class="load_in_here" id="showmeee" data-pfi-area="true">
+<div class="load_in_here" id="this_1" data-pfi-area="true">
     <h2 class="words_1">Available Now</h2>
 </div>
 
