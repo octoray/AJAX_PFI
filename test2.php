bb7ed4b1e0dee3ea169e9de2808c71191bb18e77
davidwintour <david.wintour@gmail.com>
2015-10-22 13:38:10 +0100

    Upload

--- test2.php
+++ test2.php
@@ -127,14 +127,14 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
     <button onclick="hidebutton()">Click me to run hidebutton()</button>
 
 <!-- 1st block /Purchase subscription button -->
-<div class="merchant-button btn1" id="hideme1" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="328" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/success.php" data-pfi-content-id="sub1">
+<div class="merchant-button btn1" id="hideme1" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="326" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/success.php" data-pfi-content-id="sub1">
 subscripbe to Expansion Pack For £30 <br>(Charged To Your Mobile)
 </div>
     <br>
 </div>
 
 <!-- 1st block /Purchase button -->
-<div class="merchant-button btn1" id="hideme2" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="328" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/success.php" data-pfi-content-id="<?php echo $out2;?>">
+<div class="merchant-button btn1" id="hideme2" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="330" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/success.php" data-pfi-content-id="<?php echo $out2;?>">
     Buy Expansion Pack For £30 <br>(Charged To Your Mobile)
 </div>
 <br>
