97e33d2fe168c1ff8b264fa79738971476be733c
davidwintour <david.wintour@gmail.com>
2015-10-22 14:57:16 +0100

    Upload

--- testPB.php
+++ testPB.php
@@ -1,46 +1,35 @@
-<!doctype html>
-<html ng-app="modelsModule">
-
-
+<!DOCTYPE HTML>
+<html>
 
 <head>
-    <meta name="format-detection" content="telephone=no"/>
-    <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
-    <meta charset="UTF-8"/>
+
+    <meta name="viewport" content="width=device-width">
+    <meta charset="UTF-8">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+
 </head>
+<body>
 
-
-HELLO
-<div class="container-fluid">
-    <div class="row subscribe-overlay-background">
-
-        <div data-pfi-button = "true"
-             data-pfi-button-id = "hidebutton"
-             data-pfi-service-id = "326"
-             data-pfi-reference = "1111111"
-             data-pfi-content-url = "/cams/show/28083"
-             data-pfi-content-id = "1111111"
-             ng-show="!form.confirm && subscribeResponse==0" class="col-row-12 subscribe-holder">
-
-            <img ng-click="cancelSubscribe(28083)" class="subscribe-close" src="/cams/images/exit.png" alt="close"/>
-
-            <div class="subscribe-title">BECOME A MEMBER</div>
-            <div class="subscribe-body">
-                <p class="subscribe-body-small-text">Subscribe to <span style="font-weight: bold;">69CamsBox</span></p>
-                <p class="subscribe-body-small-text">every week untill you send STOP to XXXXXX</p>
-                <div class="btn subscribe-button" ng-click="subscribe()">Subscribe now for &#163;1</div>
-                <p class="subscribe-body-large-text">Unlimited Chat</p>
-                <p>Talk to all 69CamsBox models.</p>
-                <p>Only <span style="font-weight: bold;">&#163;1 a week</span>, no ties,
-                    <span style="font-weight: bold;">stop anytime</span></p>
-            </div>
-        </div>
-
-
-        <div data-pfi-area="true" id="showmeee" >
-            <br>
-            <img src="https://www.destinythegame.com/content/dam/atvi/bungie/dtg-comet/utils/logos/destiny-taken-king-composite-logo.svg" class="banner" alt="Header"> <br>
-            <h2 class="hed">Available Now</h2>
-        </div>
-    </div>
+<!-- 1st block /Purchase button -->
+<div class="merchant-button btn1" id="content_1"
+     data-pfi-button="true"
+     data-pfi-button-id="000001"
+     data-pfi-service-id="330"
+     data-pfi-reference="987654321"
+     data-pfi-content-url="/success.php"
+     data-pfi-content-id="987654321">
+    <p>Buy For £5</p>
 </div>
+
+<br>
+
+<!-- 2nd block load stuff  -->
+<div class="load_in_here" id="showmeee" data-pfi-area="true">
+    <h2 class="words_1">Available Now</h2>
+</div>
+
+
+</body>
+</html>
