77841855bed4eb31b855a070ff79a267db7ff490
davidwintour <david.wintour@gmail.com>
2015-10-22 12:20:29 +0100

    Upload

--- testPB.php
+++ testPB.php
@@ -1,1 +1,69 @@
-sadasdasd
+
+
+
+
+<!doctype html>
+<html ng-app="modelsModule">
+
+
+
+<head>
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="contentLevel" content="soft"/>
+    <title>69CamsBox</title>
+
+    <link href="/cams/css/fonts.css" rel="stylesheet"/>
+    <link href="/cams/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+    <!-- CSS -->
+    <link href="/cams/css/pfi-header.css" rel="stylesheet"/>
+    <link href="/cams/css/custom.css" rel="stylesheet"/>
+    <link href="/cams/css/commercial.css" rel="stylesheet"/>
+
+    <script src="/cams/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/cams/bower_components/angular/angular.min.js"></script>
+    <script src="/cams/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/cams/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
+    <script src="/cams/bower_components/angular-resource/angular-resource.min.js"></script>
+    <script src="/cams/modules/users/users.js"></script>
+    <script src="/cams/modules/models/modelsNew.js"></script>
+</head>
+
+
+HELLO
+<div class="container-fluid">
+    <div class="row subscribe-overlay-background">
+
+        <div data-pfi-button = "true"
+             data-pfi-button-id = "hidebutton"
+             data-pfi-service-id = "326"
+             data-pfi-reference = "1111111"
+             data-pfi-content-url = "/cams/show/28083"
+             data-pfi-content-id = "1111111"
+             ng-show="!form.confirm && subscribeResponse==0" class="col-row-12 subscribe-holder">
+
+            <img ng-click="cancelSubscribe(28083)" class="subscribe-close" src="/cams/images/exit.png" alt="close"/>
+
+            <div class="subscribe-title">BECOME A MEMBER</div>
+            <div class="subscribe-body">
+                <p class="subscribe-body-small-text">Subscribe to <span style="font-weight: bold;">69CamsBox</span></p>
+                <p class="subscribe-body-small-text">every week untill you send STOP to XXXXXX</p>
+                <div class="btn subscribe-button" ng-click="subscribe()">Subscribe now for &#163;1</div>
+                <p class="subscribe-body-large-text">Unlimited Chat</p>
+                <p>Talk to all 69CamsBox models.</p>
+                <p>Only <span style="font-weight: bold;">&#163;1 a week</span>, no ties,
+                    <span style="font-weight: bold;">stop anytime</span></p>
+            </div>
+        </div>
+
+
+        <div data-pfi-area="true" id="showmeee" >
+            <br>
+            <img src="https://www.destinythegame.com/content/dam/atvi/bungie/dtg-comet/utils/logos/destiny-taken-king-composite-logo.svg" class="banner" alt="Header"> <br>
+            <h2 class="hed">Available Now</h2>
+        </div>
+    </div>
+</div>
