667f3cca0c0d2dfe4dbd1166c8ecb40dd65767c9
davidwintour <david.wintour@gmail.com>
2015-10-27 11:08:32 +0000

    Upload

--- staging/test2.php
+++ staging/test2.php
@@ -127,15 +127,15 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
     <button onclick="hidebutton()">STAGING Click me to run hidebutton() !!!!!!!!!!!!!</button>
 
 <!-- 1st block /Purchase subscription button -->
-<div class="merchant-button btn1" id="hideme1" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="17" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/success.php" data-pfi-content-id="<?php echo $out2;?>">
+<div class="merchant-button btn1" id="hideme1" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="326" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/success.php" data-pfi-content-id="<?php echo $out2;?>">
 subscripbe to Expansion Pack For £30 <br>(Charged To Your Mobile)
 </div>
     <br>
 </div>
 
 <!-- 1st block /Purchase button -->
-<div class="merchant-button btn1" id="hideme2" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="19" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/success.php" data-pfi-content-id="sub1">
-    Buy Expansion Pack For £30 <br>(Charged To Your Mobile)
+<div class="merchant-button btn1" id="hideme2" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="330" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/success.php" data-pfi-content-id="sub1">
+    NON SUB Buy Expansion Pack For £30 <br>(Charged To Your Mobile)
 </div>
 <br>
 </div>
