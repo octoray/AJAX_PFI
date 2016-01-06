1095c05878a9c8a93c3724cfedbab51c5d5c826a
davidwintour <david.wintour@gmail.com>
2016-01-06 13:10:59 +0000

    Upload

--- phpdb/inproc.php
+++ phpdb/inproc.php
@@ -8,7 +8,7 @@ $dbhandle = mysql_connect($hostname, $username, $password)
 or die("Unable to connect to MySQL");
 //echo "Connected to MySQL<br>";
 
-mysql_select_db("inproc") or die(mysql_error());
+mysql_select_db("ajax") or die(mysql_error());
 $r1 = array();
 // chart1
 $result1 = mysql_query('INSERT INTO `ajax`.`inproc` (`HTTP_X_PFI_SESSIONTOKEN`, `HTTP_X_PFI_STATUS`, `HTTP_X_PFI_REQUESTTIME`, `HTTP_X_PFI_HASH`, `HTTP_X_PFI_ALIAS`, `HTTP_X_PFI_NETINFO`, `HTTP_X_PFI_CALLERID`, `HTTP_X_PFI_OPTINSTATUS`, `HTTP_X_PFI_TRANSACTIONID`, `Response`)
