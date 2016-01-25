f0a764dcc86bb49aec2fdc062e242126afa27fb5
davidwintour <david.wintour@gmail.com>
2016-01-25 15:18:55 +0000

    Upload

--- staging/msisdnlookup_data.php
+++ staging/msisdnlookup_data.php
@@ -1,33 +1,4 @@
-<?php
 
-function getGUID(){
-    if (function_exists('com_create_guid')){
-        return com_create_guid();
-    }else{
-        mt_srand((double)microtime()*10000);
-        $charid = strtoupper(md5(uniqid(rand(), true)));
-        $hyphen = chr(45);
-        $uuid =
-            substr($charid, 0, 8).$hyphen
-            .substr($charid, 8, 4).$hyphen
-            .substr($charid,12, 4).$hyphen
-            .substr($charid,16, 4).$hyphen
-            .substr($charid,20,12);
-        return $uuid;
-    }
-};
-
-$GUID = getGUID();
-if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
-    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
-    $set = 'set';
-}else{
-    header("X-PFI-SessionToken: ".$GUID);
-    $notset = 'not set';
-};
-
-
-?>
 
 <script src="http://octoraypfi.co.uk/staging/jquery-2.1.4.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
@@ -41,7 +12,7 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
         dataType: "jsonp",
         data: {
             merchantToken: '57D92441-6B7F-4691-936E-10836CB92496',
-            sessionToken: '<?php echo $_SERVER['HTTP_X_PFI_SESSIONTOKEN']?>'
+            sessionToken: '92248493-2177-3DB5-463E-B6EA29ABBB67'
         }
     }).done(function (result) {
 // result contains the outcome
