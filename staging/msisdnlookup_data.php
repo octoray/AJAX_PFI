9a3f91a1360ce8df27d2e7ba41be2e58fd1396ac
davidwintour <david.wintour@gmail.com>
2016-01-25 11:01:13 +0000

    Upload

---
+++ staging/msisdnlookup_data.php
@@ -0,0 +1,48 @@
+<?php
+
+function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);
+        $uuid =
+            substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12);
+        return $uuid;
+    }
+};
+
+$GUID = getGUID();
+if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
+    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
+    $set = 'set';
+}else{
+    header("X-PFI-SessionToken: ".$GUID);
+    $notset = 'not set';
+};
+
+
+?>
+
+<script type="text/javascript">
+    $.ajax({
+        url: "http://pfi.imimobile.net/staging/Ajax/Carrier",
+        type: "POST",
+        dataType: "jsonp",
+        data: {
+            merchantToken: '57D92441-6B7F-4691-936E-10836CB92496',
+            sessionToken: '<?php echo $_SERVER['HTTP_X_PFI_SESSIONTOKEN']?>'
+        }
+    }).done(function (result) {
+// result contains the outcome
+        }).fail(function () {
+// the call was not successful
+        });
+
+
+</script>
