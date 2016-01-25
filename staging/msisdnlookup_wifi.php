d4ec58bbbc55d0db63e5203dae58d2d94bede255
davidwintour <david.wintour@gmail.com>
2016-01-25 15:51:51 +0000

    Upload

--- staging/msisdnlookup_wifi.php
+++ staging/msisdnlookup_wifi.php
@@ -1,33 +1,4 @@
-<?php
 
-function getGUID(){
-    if (function_exists('com_create_guid')){
-        return com_create_guid();
-    }else{
-        mt_srand((double)microtime()*10000);
-        $charid = strtoupper(md5(uniqid(rand(), true)));
-        $hyphen = chr(45);
-        $uuid =
-            substr($charid, 0, 8).$hyphen
-            .substr($charid, 8, 4).$hyphen
-            .substr($charid,12, 4).$hyphen
-            .substr($charid,16, 4).$hyphen
-            .substr($charid,20,12);
-        return $uuid;
-    }
-};
-
-$GUID = getGUID();
-if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
-    header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
-    $set = 'set';
-}else{
-    header("X-PFI-SessionToken: ".$GUID);
-    $notset = 'not set';
-};
-
-
-?>
 
 <script src="http://octoraypfi.co.uk/staging/jquery-2.1.4.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
@@ -36,17 +7,17 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
 
 <script type="text/javascript">
     $.ajax({
-        url: "http://pfi.imimobile.net/staging/Ajax/Lookup",
+        url: "http://pfi.imimobile.net/staging/msisdnlookup/ajax/carrier",
         type: "POST",
         dataType: "jsonp",
         data: {
-            merchantToken: '57D92441-6B7F-4691-936E-10836CB92496',
-            sessionToken: '<?php echo $GUID ?>',
-            msisdn: '447768240407'
+            merchantToken: '<?php echo $_GET['merchantToken'];?>',
+            sessionToken: '<?php echo $_GET['sessionToken'];?>',
+            msisdn: '<?php echo $_GET['msisdn'];?>'
         }
-    }).done(function (result) {
-document.write(result);
-        }).fail(function () {
+    }).done(function(result) {
             document.write(result);
+        }).fail(function() {
+            document.write('it fucked up');
         });
 </script>
