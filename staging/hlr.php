796e124f06bd3ed7864534f293923067544d3596
davidwintour <david.wintour@gmail.com>
2016-01-26 07:21:51 +0000

    Upload

--- staging/hlr.php
+++ staging/hlr.php
@@ -7,10 +7,12 @@
 </head>
 <body>
 
+
+
 <?php
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $msisdn = $comment = $website = "";
+$name = $email = $msisdn = $comment = $session = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
@@ -30,6 +32,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $msisdn = test_input($_POST["msisdn"]);
     }
 
+    if (empty($_POST["session"])) {
+        $session = "";
+    } else {
+        $session = test_input($_POST["session"]);
+    }
+
 
 }
 
@@ -44,22 +52,99 @@ function test_input($data) {
 <h2>PHP Form Validation Example</h2>
 <br>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    Name: <input type="text" name="name" value="<?php echo $name;?>">
+    Merchant Token: <input type="text" name="name" value="<?php echo $name;?>">
     <span class="error">* <?php echo $nameErr;?></span>
     <br><br>
-    Msisdn: <textarea name="msisdn" rows="5" cols="40"><?php echo $msisdn;?></textarea>
+    Session Token: <textarea name="session" rows="5" cols="40"><?php echo $session;?></textarea>
     <br><br>
+    Msisdn: <textarea name="msisdn" rows="5" cols="40"><?php echo $msisdn;?></textarea>
     <input type="submit" name="submit" value="Submit">
 </form>
 
 <?php
 echo "<h2>Your Input:</h2>";
+echo "Merchant Token";
+echo "<br>";
 echo $name;
+echo "<br>";
+echo "Session Token";
+echo "<br>";
+echo $session;
+echo "<br>";
+echo "Msisdn";
 echo "<br>";
 echo $msisdn;
 echo "<br>";
 
-?>
 
-</body>
-</html>
+function getGUID(){
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);
+        $uuid =
+            substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12);
+        return $uuid;
+    }
+};
+
+$GUID = getGUID();
+
+echo "<br>";
+echo $GUID;
+echo "<br>";
+
+
+function sendmessage($n,$s,$m) {
+    $ch = curl_init('http://pfi.imimobile.net/staging/msisdnlookup/web/lookup');
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_URL, 'http://pfi.imimobile.net/staging/msisdnlookup/web/lookup');
+    curl_setopt($ch, CURLOPT_POST, 1);
+
+    $post = array(
+        'merchantToken'=>($n),
+        'sessionToken'=>($s),
+        'msisdn'=>($m)
+    );
+
+
+
+    $query = http_build_query($post);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+
+    $runy = curl_exec($ch);
+    $info = curl_getinfo($ch);
+
+    echo '<br>';
+    echo "URL POSTED TO IS: ".$info['url'];
+//echo $info['url'];
+    echo '<br>';
+//echo '<br>';
+    echo "POSTED DATA IS:"; print '<BR>';
+    print_r($post);
+    print '<BR>';
+    echo "RESPONSE CODE: ".$info['http_code'];
+    print '<BR>';
+    echo "RESPONSE  IS:  "  .$runy;
+    print '<BR>';
+    echo "Remote IP:  ".$info['primary_ip'];
+    print '<BR>';
+    echo "Time TAken:  ".$info['total_time'];
+
+}
+
+
+if (empty($_POST["session"])) {
+} else {
+    sendmessage($name,$session,$msisdn);
+}
+?>
