edef6567bdb2dc7336227b4f1608a42a87d53c92
davidwintour <david.wintour@gmail.com>
2016-01-26 11:29:16 +0000

    Upload

--- staging/hlr.php
+++ staging/hlr.php
@@ -18,6 +18,26 @@
     <script src="http://octoraypfi.co.uk/staging/jquery-2.1.4.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
     <script src="http://octoraypfi.co.uk/staging/slicknav/dist/jquery.slicknav.js"></script>
+
+    <script type="text/javascript">
+        function callajax(callback) {
+            $.ajax({
+                url: "http://pfi.imimobile.net/staging/msisdnlookup/ajax/lookup",
+                type: "POST",
+                dataType: "jsonp",
+                data: {
+                    merchantToken: '<?php echo $_POST["name"];?>',
+                    sessionToken: '<?php echo $_POST["session"];?>',
+                    msisdn: '<?php echo $_POST["msisdn"];?>'
+                }
+            }).done(function(data) {
+                    document.write(data);
+                }).fail(function() {
+                    document.write('AJAX lookup Failed :(');
+                });}
+
+    </script>
+
 </head>
 <body>
 
@@ -166,48 +186,17 @@ function sendmessage($n,$s,$m) {
 
 }
 
-?>
-
-<script type="text/javascript">
-    function callajax(callback) {
-        $.ajax({
-            url: "http://pfi.imimobile.net/staging/msisdnlookup/ajax/lookup",
-            type: "POST",
-            dataType: "jsonp",
-            data: {
-                merchantToken: '<?php echo $_POST["name"];?>',
-                sessionToken: '<?php echo $_POST["session"];?>',
-                msisdn: '<?php echo $_POST["msisdn"];?>'
-            }
-        }).done(function(data) {
-                document.write(data);
-            }).fail(function() {
-                document.write('AJAX lookup Failed :(');
-            });}
-
-</script>
-
-<?php
 
 if (empty($_POST["session"])) {
 
 } else {
-    if (isset($ajax)){
-        echo '<script type="text/javascript">'
-        , 'callajax().success(function(data) {
-        document.write(data);
-    });'
-        , '</script>'
-        ;
-    }if (isset($http)){
-        sendmessage($name,$session,$msisdn);
-    }else{
-
-    }
+    sendmessage($name,$session,$msisdn);
 }
 
 
 ?>
+
+
 
 
 
