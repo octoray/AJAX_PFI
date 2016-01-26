eedcdd9b89e1c3c104a6fdea0ab54f8531ed622f
davidwintour <david.wintour@gmail.com>
2016-01-26 06:40:25 +0000

    Upload

---
+++ staging/hlr.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        .error {color: #FF0000;}
+    </style>
+</head>
+<body>
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $msisdn = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+            $nameErr = "Only letters and white space allowed";
+        }
+    }
+
+
+    if (empty($_POST["msisdn"])) {
+        $comment = "";
+    } else {
+        $comment = test_input($_POST["msisdn"]);
+    }
+
+
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<br>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Name: <input type="text" name="name" value="<?php echo $name;?>">
+    <span class="error">* <?php echo $nameErr;?></span>
+    <br><br>
+    Msisdn: <textarea name="msisdn" rows="5" cols="40"><?php echo $msisdn;?></textarea>
+    <br><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $msisdn;
+echo "<br>";
+
+?>
+
+</body>
+</html>
