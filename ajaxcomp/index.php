970af17aa78ac7b8361fec55971cb95f440fa5e6
davidwintour <david.wintour@gmail.com>
2016-01-28 15:25:35 +0000

    Upload

--- ajaxcomp/index.php
+++ ajaxcomp/index.php
@@ -61,8 +61,9 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
         <p class="stuff1">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 
         <br>
+<div class="btn2">
         <a href="http://octoraypfi.co.uk/ajaxcomp/start.php" id="btn2">PLAY NOW</a>
-
+</div>
 
 </body>
 
