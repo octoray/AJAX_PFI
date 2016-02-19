640730a89c434fb9b56bc4ba97897fedbb564bcd
davidwintour <david.wintour@gmail.com>
2016-02-19 15:01:21 +0000

    Upload

--- staging/pAPI_JS.php
+++ staging/pAPI_JS.php
@@ -9,7 +9,10 @@
     var start = new Date();
     $.ajax({
         url: "http://pfi.imimobile.net/api/PersonalLink.svc/rest/SendAjaxPersonalLink",
-        headers: { 'Authorization': 'Basic MTpBRTdBMDdBOC1EMDg1LTQwQzYtQUNFNC1GRjg0RjE0RTMyNDc=' },
+        beforeSend: function (request)
+        {
+            request.setRequestHeader("Authorization", "Basic MTpBRTdBMDdBOC1EMDg1LTQwQzYtQUNFNC1GRjg0RjE0RTMyNDc=");
+        },
         type: "POST",
         dataType: "jsonp",
         data: {"SessionToken":"4AD263B3-C0F5-8C0F-0AD0-380748666523","MSISDN":"447768240407","DestinationUrl":"http://the-sloth.uk/","Message":"test JS {url}","Spoof":"62277"}
