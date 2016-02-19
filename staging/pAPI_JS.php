415126b551995de0afccfd2b708900a24722edc5
davidwintour <david.wintour@gmail.com>
2016-02-19 15:22:16 +0000

    Upload

--- staging/pAPI_JS.php
+++ staging/pAPI_JS.php
@@ -5,23 +5,23 @@
 <script src="http://octoraypfi.co.uk/staging/slicknav/dist/jquery.slicknav.js"></script>
 
 
-<script type="text/javascript">
-    var start = new Date();
-    $.ajax({
-        url: "http://pfi.imimobile.net/api/PersonalLink.svc/rest/SendAjaxPersonalLink",
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Basic MTpBRTdBMDdBOC1EMDg1LTQwQzYtQUNFNC1GRjg0RjE0RTMyNDc=");
-        },
-        headers: { 'Authorization': 'Basic MTpBRTdBMDdBOC1EMDg1LTQwQzYtQUNFNC1GRjg0RjE0RTMyNDc=' },
-        type: "POST",
-        dataType: "jsonp",
-        data: {"SessionToken":"4AD263B3-C0F5-8C0F-0AD0-380748666523","MSISDN":"447768240407","DestinationUrl":"http://the-sloth.uk/","Message":"test JS {url}","Spoof":"62277"}
-    }).done(function(result) {
-            document.write(result);
-        }).fail(function() {
-            document.write('Error');
-        });
-</script>
 
+
+
+<script type="text/javascript">
+$.ajax({
+url: 'http://pfi.imimobile.net/api/PersonalLink.svc/rest/SendAjaxPersonalLink',
+headers: {
+'Authorization':'Basic MTpBRTdBMDdBOC1EMDg1LTQwQzYtQUNFNC1GRjg0RjE0RTMyNDc=',
+'Content-Type':'application/json'
+},
+method: 'POST',
+dataType: 'json',
+    data: {"SessionToken":"4AD263B3-C0F5-8C0F-0AD0-380748666523","MSISDN":"447768240407","DestinationUrl":"http://the-sloth.uk/","Message":"test JS {url}","Spoof":"62277"},
+success: function(data){
+console.log('succes: '+data);
+}
+});
+</script>
 
 
