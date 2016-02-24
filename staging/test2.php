b9b80420e79ef52402b187d0afe6950dec12658e
davidwintour <david.wintour@gmail.com>
2016-02-24 11:30:10 +0000

    Upload

--- staging/test2.php
+++ staging/test2.php
@@ -48,16 +48,8 @@
 
 
 <!-- 1st block /Destiny logo text overlay -->
-<div class="block_11">
-<img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/banner.png" class="banner" alt="Header">
-<br>
-    <!-- <a href="sms:/* phone number here */?body=/* body text here */">Link</a>-->
-    <br><br><br>
-
-
-
-
-
+<div class="block_2">
+    <img src="http://images.thisisxbox.com/2013/12/destiny-logo.jpg" class="image2" alt="Header" width="100%">
 </div>
 
 <!-- 2nd block  -->
@@ -94,72 +86,7 @@
 
 
 
-
-<!-- 3nd block  -->
-<div class="block_2">
-    <img src="http://images.thisisxbox.com/2013/12/destiny-logo.jpg" class="image2" alt="Header" width="100%">
-</div>
-
-
-<!-- 4th block  -->
-<div class="block_6">
-    <br>
-    <h1 class="hed">The Taken King Live Action Trailer – Evil’s Most Wanted</h1> <br>
-    You killed his son. Now Oryx, The Taken King, wants revenge. Gather your Fireteam and take on the greatest threat this universe has seen with Destiny: The Taken King.
-    <br><br><br>
-</div>
-
-
-<!-- 5th block  -->
-<div class="block_4">
-    <img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/box1.jpg" class="image2" alt="Header" width="100%">
-</div>
-
-
-<!-- 6th block  -->
-<div class="block_6">
-    <br>
-    <h1 class="hed">The Taken King - Legendary Edition</h1> <br>
-    Destiny: The Taken King - Legendary Edition is perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legends in the stars.
-    <br><br><br>
-</div>
-
-
-<!-- 7th block  -->
-<div class="block_4">
-    <img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/image2.png" class="image2" alt="Header" width="100%">
-</div>
-
-
-<!-- 8th block  -->
-<div class="block_6"><br>
-    <h1 class="hed">THIS IS YOUR LEGEND</h1> <br>
-    ACCESS YOUR ARSENAL, TRACK YOUR STATS, AND TEAM UP WITH OTHER GUARDIANS.
-    <br><br><br>
-</div>
-
-<div class="block_5">
-    <img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/gif.gif" class="image2" alt="Header" width="100%">
-</div>
-
-
-<div class="block_6">
-    <h1 class="edition-title hed" >
-        The Taken King - <br>Legendary Edition
-    </h1><br>
-    Destiny: &nbsp;The Taken King -&nbsp;Legendary Edition is&nbsp;perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legend in the stars. &nbsp;The Legendary Edition features the complete Destiny experience including The Taken King, the next evolution in the Destiny universe, Destiny, Expansion I: The Dark Below and Expansion II: House of Wolves. &nbsp;Unite with 20 million Guardians in the fight to defeat Earth’s enemies, reclaim all that we have lost, and Become Legend.
-<br><br><br><br>
-    <!-- 1st block /Purchase button -->
-
-    <br>
-</div>
-    <br><br>
-<div class="btn1" onclick="clickme()">
-    hide top buttons
-</div>
-    <br>
-</div>
-
+<br>
 
 
 <div class="footer">
