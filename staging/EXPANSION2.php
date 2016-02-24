35b338a5b7b05d65af5f0aafa61b5f18ef66fe75
davidwintour <david.wintour@gmail.com>
2016-02-24 13:07:31 +0000

    Upload

--- staging/EXPANSION2.php
+++ staging/EXPANSION2.php
@@ -69,8 +69,8 @@
 <!-- 4th block  -->
 <div class="block_6">
     <br>
-    <h1 class="hed">The Taken King Live Action Trailer – Evil’s Most Wanted</h1> <br>
-    You killed his son. Now Oryx, The Taken King, wants revenge. Gather your Fireteam and take on the greatest threat this universe has seen with Destiny: The Taken King.
+    <h1 class="hed">HOUSE OF WOLVES</h1> <br>
+    Expand your Destiny adventure with a wealth of weapons, armor, and gear to earn in new story missions, three new competitive multiplayer maps, and a new cooperative Strike. Expansion II introduces a new competitive elimination mode in the Crucible and an all-new arena activity – The Prison of Elders. The Reef is open. Join the Awoken and hunt down the Fallen rising against us.
     <br><br><br>
 </div>
 
@@ -84,15 +84,15 @@
 <!-- 6th block  -->
 <div class="block_6">
     <br>
-    <h1 class="hed">The Taken King - Legendary Edition</h1> <br>
-    Destiny: The Taken King - Legendary Edition is perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legends in the stars.
+    <h1 class="hed">HOUSE OF WOLVES</h1> <br>
+    New weapons, armor, and gear to increase your Guardian’s power, including Fallen and Osiris-themed collections earned as top-tier rewards in new activities. New Activity rewards will enable Guardians to upgrade old favorites. Legendary and Exotic gear from Destiny, The Dark Below, and House of Wolves can be boosted to the current maximum Attack, Defense, and Light values.
     <br><br><br>
 </div>
 
 
 <!-- 7th block  -->
 <div class="block_4">
-    <img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/image2.png" class="image2" alt="Header" width="100%">
+    <img src="http://images.techtimes.com/data/images/full/85113/destiny-house-of-wolves.jpg?w=600" class="image2" alt="Header" width="100%">
 </div>
 
 
@@ -104,15 +104,15 @@
 </div>
 
 <div class="block_5">
-    <img src="http://<?php echo $_SERVER['SERVER_NAME'];?>/staging/images/gif.gif" class="image2" alt="Header" width="100%">
+    <img src="http://cdn.idigitaltimes.com/sites/idigitaltimes.com/files/styles/large/public/2015/04/22/destiny-house-wolves.jpg" alt="Header" width="100%">
 </div>
 
 
 <div class="block_6">
     <h1 class="edition-title hed" >
-        The Taken King - <br>Legendary Edition
+        THOUSE OF WOLVES
     </h1><br>
-    Destiny: &nbsp;The Taken King -&nbsp;Legendary Edition is&nbsp;perfect for new and existing Guardians that are ready to jump into the Destiny universe or continue to carve their legend in the stars. &nbsp;The Legendary Edition features the complete Destiny experience including The Taken King, the next evolution in the Destiny universe, Destiny, Expansion I: The Dark Below and Expansion II: House of Wolves. &nbsp;Unite with 20 million Guardians in the fight to defeat Earth’s enemies, reclaim all that we have lost, and Become Legend.
+    DThe Guardians have grown strong, and now the Queen of the Awoken has called upon them to hunt down the House of Wolves, Fallen subjects once loyal to the Queen before their violent rebellion. Meet with the Queen’s emissaries at Vestian Outpost in the Reef. Hunt down their Fallen betrayers and earn great rewards.
     <br><br><br><br>
     <!-- 1st block /Purchase button -->
 
