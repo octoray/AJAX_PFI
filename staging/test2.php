9e0bb1ddaeae582a38eeef17c0842ab38883038e
davidwintour <david.wintour@gmail.com>
2016-02-24 11:43:01 +0000

    Upload

--- staging/test2.php
+++ staging/test2.php
@@ -79,7 +79,6 @@
     <div class="merchant-button btn1" id="hideme4" data-pfi-button="true" data-pfi-button-id="hidebutton" data-pfi-service-id="337" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/success.php" data-pfi-content-id="<?php echo $out2;?>">
         New Test / single buy
     </div>
-    <br>
 </div>
 
 
