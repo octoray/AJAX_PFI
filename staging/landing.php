1e37b262edfede0e6e389c0a58deaa1af840aee9
davidwintour <david.wintour@gmail.com>
2016-02-24 10:58:01 +0000

    Upload

--- staging/landing.php
+++ staging/landing.php
@@ -163,97 +163,20 @@
 
     <br>
 </div>
-<br><br>
-<div class="btn1" onclick="clickme()">
-    hide top buttons
-</div>
 <br>
-</div>
+<br>
+<br>
+
 
 
 
 <div class="footer">
     <br>
     <img src="https://www.destinythegame.com/content/dam/atvi/global/firstparty/activision/activision-logo.svg"  class="foot_logo" alt="Header"><br><br>
-    <?php
-    echo"<br>";
-    echo $set;
-    echo $notset;
-    echo"<br>";
-    echo "Your Network is: ".$_SERVER['HTTP_X_PFI_NETINFO'];
-    echo"<br>";
-    echo "Your Alias is: ".$_SERVER['HTTP_X_PFI_ALIAS'];
-    echo"<br>";
-    echo "Your Status is: ".$_SERVER['HTTP_X_PFI_STATUS'];
-    echo"<br>";
-    echo "Your Network is: ".$_SERVER['HTTP_X_PFI_NETINFO'];
-    echo"<br>";
-    echo "Your referance is: ".$_SERVER['HTTP_X_PFI_REFERANCE'];
-    echo"<br>";
-    echo "Your Session token is: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN'];
-    echo"<br>";
-    echo "GUID generated is: ".$GUID;
-    echo"<br>";
-    echo "Your hash is: ".$_SERVER['HTTP_X_PFI_HASH'];
-    echo"<br>";
-    echo "Your callerid is: ".$_SERVER['HTTP_X_PFI_CALLERID'];
-    echo"<br>";
-    echo "Your transid is: ".$_SERVER['HTTP_X_PFI_TRANSACTIONID'];
-    echo"<br>";
-    echo"<br>";
-    echo"<br>";
-    echo print_r($_SERVER);
-    echo"<br>";
-    echo"<br>";
-    echo"response header";
-    echo print_r($http_response_header);
-    echo"<br>";
-    echo"Request";
-    echo print_r($_REQUEST);
+    <?php require("./phpdb/phpdebug.php"); ?>
 
-    //Write logs
-    $arr = $_SERVER;
-    // echo implode(" ",$arr);
-    $today1 = date("Y-m-d H:i:s");
-    $myFile = "mainpage.txt";
-    $fh = fopen($myFile, 'a') or die("can't open file");
-    $stringData = "\n";
-    fwrite($fh, $stringData);
-    fwrite($fh, $today1. ": headers: " .implode(" ",$arr));
-    fclose($fh);
-
-
-    ?>
 </div>
 <?php require("./phpdb/mainpage.php"); ?>
 
 </body>
-<script>
-    function wrong() {
-        document.getElementById('buttons1').style.display = 'none';
-        document.getElementById('buttons2').style.display = 'none';
-        document.getElementById('buttons3').style.display = 'none';
-        document.getElementById('wrong').style.display = '';
-        document.getElementById('shirwan').style.display = 'none';
-        document.getElementById('question').style.display = 'none';
-    }
-
-</script>
-
-<script>
-    function correct() {
-        document.getElementById('buttons1').style.display = 'none';
-        document.getElementById('buttons2').style.display = 'none';
-        document.getElementById('buttons3').style.display = 'none';
-        document.getElementById('correct').style.display = '';
-        document.getElementById('question').style.display = 'none';
-    }
-
-</script>
-<script>
-    function nope() {
-
-    }
-
-</script>
 </html>
