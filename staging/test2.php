c9a2a2fa7d133ebab05d30699f19ed73fdedbe54
davidwintour <david.wintour@gmail.com>
2016-02-24 13:17:14 +0000

    Upload

--- staging/test2.php
+++ staging/test2.php
@@ -33,19 +33,18 @@
 <ul id="menu">
     <li>Offers
         <ul>
-            <li><a href="EXPANSION1.php">EXPANSION I Offer</a></li>
-            <li><a href="EXPANSION2.php">EXPANSION II Offer</a></li>
+            <li><a href="/staging/EXPANSION1.php">EXPANSION I Offer</a></li>
+            <li><a href="/staging/EXPANSION2.php">EXPANSION II Offer</a></li>
         </ul>
     </li>
     <li>The Taken King Offer
         <ul>
-            <li><a href="abilitys.php">Abilitys</a></li>
-            <li><a href="newgear.php">New Gear</a></li>
-            <li><a href="subclass.php">New Subclasses</a></li>
+            <li><a href="/staging/abilitys.php">Abilitys</a></li>
+            <li><a href="/staging/newgear.php">New Gear</a></li>
+            <li><a href="/staging/subclass.php">New Subclasses</a></li>
         </ul>
     </li>
 </ul>
-
 
 <!-- 1st block /Destiny logo text overlay -->
 <div class="block_2">
