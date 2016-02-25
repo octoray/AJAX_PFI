35b59e184763fa139f8b145c5b4981e951b1c527
davidwintour <david.wintour@gmail.com>
2016-02-25 12:02:28 +0000

    Upload

--- staging/wine.php
+++ staging/wine.php
@@ -59,16 +59,16 @@
 <div class="block_6667" data-pfi-area="true" id="showmeee" style="<?php if ($_SERVER['HTTP_X_PFI_STATUS'] == 'PfiHybridSession') {echo "display:none";}else{echo "";}; ?>" >
     <div class="competition">
         <div id="comp-entry-area">
-            <p class="question" id="question">How much better is Arsenal than Man Utd?</p>
+            <p class="question" id="question">When Was The TAken King Released In The UK?</p>
 
             <div class="btn1c" id="buttons1" data-pfi-button="true" data-pfi-button-id="a1" data-pfi-service-id="357" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/finish.php" data-pfi-content-id="a_01">
-                <span>10%</span>
+                <span>September 15, 2015</span>
             </div>
             <div class="btn1c" id="buttons2" data-pfi-button="true" data-pfi-button-id="a2" data-pfi-service-id="357" data-pfi-reference="<?php echo $out2;?>" data-pfi-content-url="/staging/finish.php" data-pfi-content-id="a_02">
-                <span>20%</span>
+                <span>November 15, 2013</span>
             </div>
             <div class="btn1c" id="buttons3" data-pfi-button="true" data-pfi-button-id="a3" data-pfi-service-id="357" data-pfi-reference="<?php echo $out3;?>" data-pfi-content-url="/staging/finish.php" data-pfi-content-id="a_03">
-                <span>5000%</span>
+                <span>January 15, 2014</span>
             </div>
         </div>
 
