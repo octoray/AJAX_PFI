78b45df08b455358efa0c4e90808136598cbd460
davidwintour <david.wintour@gmail.com>
2016-02-25 14:02:11 +0000

    Upload

--- staging/papijs.php
+++ staging/papijs.php
@@ -1,8 +1,9 @@
 
 
-<script src="http://octoraypfi.co.uk/staging/jquery-2.1.4.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-<script src="http://octoraypfi.co.uk/staging/slicknav/dist/jquery.slicknav.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 
 <script type="text/javascript">
         $.ajax({
