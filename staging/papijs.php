3e1757e8c12ae6762ee19fdefe61f43f2b25e3e9
octoray <david.wintour@gmail.com>
2016-02-26 10:28:36 +0000

    Update papijs.php

--- staging/papijs.php
+++ staging/papijs.php
@@ -13,10 +13,10 @@
             data: {
                 merchantToken: "57D92441-6B7F-4691-936E-10836CB92496",
                 sessionToken: "EC465AB0-A01E-CB1C-816F-6B42056CCB6C",
-                msisdn: "447961832063",
+                msisdn: "447768240407",
                 destinationUrl: "http://octoray.org.uk/staging",
-                message: "Super test 12123423542345345435345 34sddhsdjfafhkjgdfjhkgfdajhkgfdajhkgdfajhkgdfasjhkgfdasjhkgdfasjhkgfjdjjghkdsfjghkasdfjghkadfsjghkdfsajghkfdas {url}",
-                spoof: "334466"
+                message: "Super test 12123423542345345435345 das !£$%%^&**) {url}",
+                spoof: "personal"
             }
         }).done(function (result) {
                 document.write(result);
