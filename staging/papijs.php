626cc6843dd56741c9d6647d262bf8161c8ab006
octoray <david.wintour@gmail.com>
2016-02-26 14:38:11 +0000

    Update papijs.php

--- staging/papijs.php
+++ staging/papijs.php
@@ -7,14 +7,14 @@
 
 <script type="text/javascript">
         $.ajax({
-            url: "http://pfi.imimobile.net/staging/msisdnlookup/Ajax/SendAjaxPersonalLink",
+            url: "http://pfi.imimobile.net/msisdnlookup/Ajax/SendAjaxPersonalLink",
             type: "POST",
             dataType: "jsonp",
             data: {
-                merchantToken: "57D92441-6B7F-4691-936E-10836CB92496",
+                merchantToken: "88209BE0-D22D-4CC4-9B3B-6A5AC736D877",
                 sessionToken: "A88DAC9A-3A7C-41EC-87B0-FE91E4019AF4",
                 msisdn: "447768240407",
-                destinationUrl: "http://octoray.org.uk/staging",
+                destinationUrl: "http://pfi.69camsbox.com/cams",
                 message: "Super test 12123423542345345435345 das !£$%%^&**) {url}",
                 spoof: "personal"
             }
