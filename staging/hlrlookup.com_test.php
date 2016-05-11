3e1f8e6f65cf9db450b1b240f1ca88f8dea8c6b7
davidwintour <david.wintour@gmail.com>
2016-05-11 16:08:52 +0100

    change staging to live PP for testing

--- staging/hlrlookup.com_test.php
+++ staging/hlrlookup.com_test.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    $curl = curl_init("https://hlrlookup.com/api/hlr/?apikey=BebTIXVVcUP8kQIRQU6PQmIBNZBkLFgN&password=Wyab9mgx&msisdn=447768240407");
+    $curl = curl_init("http://google.com/api/hlr/?apikey=BebTIXVVcUP8kQIRQU6PQmIBNZBkLFgN&password=Wyab9mgx&msisdn=447768240407");
     curl_setopt($curl, CURLOPT_HEADER,0);
     curl_setopt($curl, CURLOPT_POST, 0);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
