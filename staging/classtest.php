b78b7697f6f4e99343655a2694b7527bab5a1656
davidwintour <david.wintour@gmail.com>
2016-06-16 14:38:46 +0100

    change staging to live PP for testing

--- staging/classtest.php
+++ staging/classtest.php
@@ -3,12 +3,15 @@
 class MyClass
 {
     // Class properties and methods go here
-    public $prop1 = "I'm a class property!";
+    public $prop1 = "I'm a first class property!";
+    public $prop2 = "I'm a second class property!";
+
 }
 
 $obj = new MyClass;
 
 echo $obj->prop1;
-echo "<br>"
+echo "<br>";
+echo $obj->prop2;
 
 ?>
