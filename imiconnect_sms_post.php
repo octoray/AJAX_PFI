4c80c52fe4af0283b4d88fa0ada41e9d8a9993c1
davidwintour <david.wintour@gmail.com>
2016-07-26 14:59:44 +0100

    patch test

--- imiconnect_sms_post.php
+++ imiconnect_sms_post.php
@@ -30,7 +30,7 @@ if(isset($_POST['url']))
     $request_headers[] = 'Accept: application/json';
     $request_headers[] = 'Accept-Charset: UTF-8';
     $request_headers[] = 'Encoding: UTF-8';
-    $request_headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    $request_headers[] = 'Content-Type: application/json';
 
     $ch = curl_init($_POST['url']);
     curl_setopt($ch, CURLOPT_HEADER, true);
