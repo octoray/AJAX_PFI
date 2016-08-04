b9604efb7100441c76088b3594fd039ea744011f
davidwintour <david.wintour@gmail.com>
2016-08-04 14:01:30 +0100

    patch test

--- std_test.php
+++ std_test.php
@@ -27,10 +27,10 @@ style.textContent =
     'body {' +
         '  background-color: #33a9a9; !important;' +
         '  background-image: some-image;' +
-        '}'
-;
+        '}';
 iframe.contentDocument.head.appendChild(style);
 </script>
+
 
 <a href="http://pfi.imimobile.net/staging/checkout/?sid=344&ref=IMI_00000000000_2">PFI-s normal http non adult</a>
 <br>
@@ -42,29 +42,15 @@ iframe.contentDocument.head.appendChild(style);
 <br>
 <br>
 <br>
-<iframe id="frame" class="frame" style="width: 420px; height: 280px; background-color: powderblue;" src="http://pfi.imimobile.net/staging/checkout/?sid=344&ref=0000000101&render=embed" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+<iframe id="frame1" class="frame1" style="width: 420px; height: 280px; background-color: powderblue;" src="http://pfi.imimobile.net/staging/checkout/?sid=344&ref=0000000101&render=embed" marginheight="0" marginwidth="0" frameborder="0"></iframe>
 
 <br>
 <br>
 LIVE
 <br>
-<iframe id="frame" class="frame" style="width: 420px; height: 280px; background-color: powderblue;" src="http://pfi.imimobile.net/checkout/?sid=115&ref=0000000101&render=embed" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+<iframe id="frame2" class="frame2" style="width: 420px; height: 280px; background-color: powderblue;" src="http://pfi.imimobile.net/checkout/?sid=115&ref=0000000101&render=embed" marginheight="0" marginwidth="0" frameborder="0"></iframe>
 
 
 <script>
-    $('iframe').load( function() {
-        $('iframe').contents().find("head")
-            .append($("<style type='text/css'>  input.btn, a.btn{display:none;!important;}  </style>"));
-    });
-
-
-    var iframe = document.getElementById('frame');
-    var style = document.createElement('style');
-    style.textContent =
-        'body {' +
-            '  background-color: #33a9a9; !important;' +
-            '  background-image: some-image; !important;' +
-            '}'
-    ;
-    iframe.contentDocument.head.appendChild(style);
+    document.getElementById('frame1').style.display = 'none';
 </script>
