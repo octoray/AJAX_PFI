bebc258d9b47a734c62005aba5f3256636931eb1
davidwintour <david.wintour@gmail.com>
2016-08-04 11:20:28 +0100

    patch test

--- msisdnlookup_wifi.php
+++ msisdnlookup_wifi.php
@@ -13,7 +13,7 @@
         data: {
             merchantToken: 'AE7A07A8-D085-40C6-ACE4-FF84F14E3247',
             sessionToken: '70ACC6D7-7715-FA5B-BA98-B0FA35DA1C8F',
-            msisdn: '447816164026'
+            msisdn: '447920201277'
         }
     }).done(function(result) {
             document.write(result);
