0d3235853e3fd9571c838fe69170b8a48b60e239
davidwintour <david.wintour@gmail.com>
2016-08-11 14:15:55 +0100

    patch test

--- staging/phpdb/phpgeneralfun.php
+++ staging/phpdb/phpgeneralfun.php
@@ -20,10 +20,8 @@ function getGUID(){
 $GUID = getGUID();
 if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
     header("X-PFI-SessionToken: ".$_SERVER['HTTP_X_PFI_SESSIONTOKEN']);
-    $set = 'set';
 }else{
     header("X-PFI-SessionToken: ".$GUID);
-    $notset = 'not set';
 };
 
 
