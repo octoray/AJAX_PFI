7ea87c1795d592475ba7e733352cb32103ff20db
davidwintour <david.wintour@gmail.com>
2016-08-11 14:00:58 +0100

    patch test

---
+++ imiconnect_test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width">
+    <meta charset="UTF-8">
+    <!-- activate Menu -->
+
+<?php
+?>
+<h1> Options </h1>
+<p>
+<a href="http://octoraypfi.co.uk/imiconnect_sms_post.php">SMS Send API</a>
+Try it »
+</p>
+
+<p>
+    <a href="http://octoraypfi.co.uk/imiconnect_sms_post.php">Facebook Send API</a>
+    Try it »
+</p>
