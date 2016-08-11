3ec3c848bd69eaa47829fdb9b48f58b9d5521a19
davidwintour <david.wintour@gmail.com>
2016-08-11 15:06:11 +0100

    patch test

--- imiconnect_twitter_post.php
+++ imiconnect_twitter_post.php
@@ -43,23 +43,19 @@ if(isset($_POST['url']))
 
     $post_json = '
     {
-	"notifyurl": "'.$_POST['notifyurl'].'",
-	"channels": {
-		"sms": {
-			"senderid": "'.$_POST['senderid'].'",
-			"text": "'.$_POST['text'].'",
-			"type": "'.$_POST['type'].'"
-		}
-	},
-	"deliverychannel": "'.$_POST['deliverychannel'].'",
-	"correlationid": "'.$_POST['correlationid'].'",
-	"destination": [
-		{
-			"customerid": [
-				"'.$_POST['customerid'].'"
-			]
-		}
-	]
+  "deliverychannel": "'.$_POST['deliverychannel'].'",
+  "channels": {
+        "OTT-Messaging": {
+            "fb": {
+                "text": "'.$_POST['text'].'"
+            }
+        }
+    },
+    "destination": [{
+        "psid": [
+            "'.$_POST['psid'].'"
+        ]
+    }]
 }
     ';
 
@@ -90,7 +86,8 @@ if(isset($_POST['url']))
 
 // Perform queries
 
-    mysqli_query($con,"INSERT INTO sms_sent (header_key,notifyurl,senderid,msg,j_type,deliverychannel,correlationid,customerid,response_code,total_time_taken,namelookup_time,connect_time,response_json) VALUES ('".$_POST['key_header']."','".$_POST['notifyurl']."','".$_POST['senderid']."','".$_POST['text']."','".$_POST['type']."','".$_POST['deliverychannel']."','".$_POST['correlationid']."','".$_POST['customerid']."','".$info['http_code']."','".$info['total_time']."','".$info['namelookup_time']."','".$info['connect_time']."','".$runy."')");
+    //mysqli_query($con,"INSERT INTO fb_sent (header_key,deliverychannel,msg,psid,response_code,total_time_taken,namelookup_time,connect_time,response_json) VALUES ('".$_POST['key_header']."','".$_POST['notifyurl']."','".$_POST['senderid']."','".$_POST['text']."','".$_POST['type']."','".$_POST['deliverychannel']."','".$_POST['correlationid']."','".$_POST['customerid']."','".$info['http_code']."','".$info['total_time']."','".$info['namelookup_time']."','".$info['connect_time']."','".$runy."')");
+    mysqli_query($con,"INSERT INTO twitter_sent (header_key,msg,deliverychannel,twitterid,response_code,total_time_taken,namelookup_time,connect_time,response_json)VALUES('".$_POST['key_header']."','".$_POST['text']."','".$_POST['deliverychannel']."','".$_POST['twitterid']."','".$info['http_code']."','".$info['total_time']."','".$info['namelookup_time']."','".$info['connect_time']."','".$runy."')");
     mysqli_close($con);
 }
 
@@ -99,153 +96,120 @@ if(isset($_POST['url']))
 ?>
 
 <div id="chartContainer_header" align="center" class="chart1">
-<li><h1>IMIConnect Twitter POST</h1></li>
-<br>
-<form name="htmlform" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <li><h1>IMIConnect Twitter POST</h1></li>
+    <br>
+    <form name="htmlform" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <table width="550px">
+            </tr>
+
+            <tr>
+                <td valign="top">
+                    <label for="server">URL</label>
+                </td>
+                <td valign="top">
+                    <input  type="text" name="url" maxlength="80" size="50" value="<?php if(isset($_POST['url'])) {echo $_POST['url'];}else{echo "https://api.imiconnect.com/resources/v1/messaging";};?>">
+                </td>
+            </tr>
+            <tr>
+                <td valign="top">
+                    <label for="server">Header (key)</label>
+                </td>
+                <td valign="top">
+                    <input  type="text" name="key_header" maxlength="80" size="50" value="<?php if(isset($_POST['key_header'])) {echo $_POST['key_header'];}else{echo "ba8ca2eb-3ea9-11e6-b778-005056ad794f";};?>">
+                </td>
+            </tr>
+
+            <tr>
+                <td valign="top">
+                    <label for="ip">BODY (deliverychannel)</label>
+                </td>
+                <td valign="top">
+                    <input  type="text" name="deliverychannel" maxlength="80" size="50" value="<?php if(isset($_POST['deliverychannel'])) {echo $_POST['deliverychannel'];}else{echo "fb";};?>"><br><br><br>
+                </td>
+            </tr>
+
+
+            </tr>
+            <tr>
+                <td valign="top">
+                    <label for="server">BODY (text)</label>
+                </td>
+                <td valign="top">
+                    <input  type="text" name="text" maxlength="80" size="50" value="<?php if(isset($_POST['text'])) {echo $_POST['text'];}else{echo "IMIconnect FB Test 000001";};?>">
+                </td>
+            </tr>
+
+
+
+            <tr>
+                <td valign="top">
+                    <label for="ip">BODY (twitterid)</label>
+                </td>
+                <td valign="top">
+                    <input  type="text" name="twitterid" maxlength="80" size="20" value="<?php if(isset($_POST['twitterid'])) {echo $_POST['twitterid'];}else{echo "xxxxxxxxxxxx";};?>">
+                </td>
+            </tr>
+
+
+
+            <tr>
+                <td colspan="2" style="text-align:center">
+                    <input type="submit" value="Send">
+                </td>
+            </tr>
+        </table>
+    </form>
+    <br><br>
+
+    <p><li><h1>Output</h1></li>
+    <br>
     <table width="550px">
-        </tr>
 
         <tr>
             <td valign="top">
-                <label for="server">URL</label>
+                <label for="details">JSON Sent</label>
             </td>
             <td valign="top">
-                <input  type="text" name="url" maxlength="80" size="50" value="<?php if(isset($_POST['url'])) {echo $_POST['url'];}else{echo "https://api.imiconnect.com/resources/v1/messaging";};?>">
-            </td>
-        </tr>
-        <tr>
-            <td valign="top">
-                <label for="server">Header (key)</label>
-            </td>
-            <td valign="top">
-                <input  type="text" name="key_header" maxlength="80" size="50" value="<?php if(isset($_POST['key_header'])) {echo $_POST['key_header'];}else{echo "ba8ca2eb-3ea9-11e6-b778-005056ad794f";};?>">
+                <textarea  name="details" maxlength="500" cols="99" rows="20"><?php echo $post_json ?></textarea>
+                </textarea>
             </td>
         </tr>
 
         <tr>
             <td valign="top">
-                <label for="ip">BODY (notifyurl)</label>
+                <label for="details">Values Set</label>
             </td>
             <td valign="top">
-                <input  type="text" name="notifyurl" maxlength="80" size="50" value="<?php if(isset($_POST['notifyurl'])) {echo $_POST['notifyurl'];}else{echo "";};?>"><br><br><br>
-            </td>
-        </tr>
-
-        <tr>
-            <td valign="top"">
-            <label for="login">BODY (senderid)</label>
-            </td>
-            <td valign="top">
-                <input  type="text" name="senderid" maxlength="80" size="50"value="<?php if(isset($_POST['senderid'])) {echo $_POST['senderid'];}else{echo "CONNCT";};?>">
-            </td>
-        </tr>
-
-        </tr>
-        <tr>
-            <td valign="top">
-                <label for="server">BODY (text)</label>
-            </td>
-            <td valign="top">
-                <input  type="text" name="text" maxlength="80" size="50" value="<?php if(isset($_POST['text'])) {echo $_POST['text'];}else{echo "IMIconnect Test 000001";};?>">
+                <textarea  name="details" maxlength="500" cols="99" rows="20"><?php print_r($_POST);?></textarea>
+                </textarea>
             </td>
         </tr>
 
         <tr>
             <td valign="top">
-                <label for="ip">BODY (type)</label>
+                <label for="details">RESPONSE</label>
             </td>
             <td valign="top">
-                <input  type="text" name="type" maxlength="80" size="50" value="<?php if(isset($_POST['type'])) {echo $_POST['type'];}else{echo "1";};?>"><br><br><br>
+                <textarea  name="details" maxlength="500" cols="99" rows="20">
+                    <?php
+
+                    print_r($info);
+
+
+                    print_r($runy);
+
+                    ?>
+                </textarea>
+                </textarea>
             </td>
         </tr>
 
-        <tr>
-            <td valign="top">
-                <label for="ip">BODY (deliverychannel)</label>
-            </td>
-            <td valign="top">
-                <input  type="text" name="deliverychannel" maxlength="80" size="50" value="<?php if(isset($_POST['deliverychannel'])) {echo $_POST['deliverychannel'];}else{echo "sms";};?>">
-            </td>
-        </tr>
-
-        <tr>
-            <td valign="top">
-                <label for="ip">BODY (correlationid)</label>
-            </td>
-            <td valign="top">
-                <input  type="text" name="correlationid" maxlength="80" size="50" value="<?php if(isset($_POST['correlationid'])) {echo $_POST['correlationid'];}else{echo $GUID;};?>"><br><br><br>
-            </td>
-        </tr>
-
-        <tr>
-            <td valign="top">
-                <label for="ip">BODY (customerid)</label>
-            </td>
-            <td valign="top">
-                <input  type="text" name="customerid" maxlength="80" size="20" value="<?php if(isset($_POST['customerid'])) {echo $_POST['customerid'];}else{echo "0000001";};?>">
-            </td>
-        </tr>
-
-
-
-        <tr>
-            <td colspan="2" style="text-align:center">
-                <input type="submit" value="Send">
-            </td>
-        </tr>
     </table>
-</form>
-<br><br>
-
-<p><li><h1>Output</h1></li>
-<br>
-<table width="550px">
-
-    <tr>
-        <td valign="top">
-            <label for="details">JSON Sent</label>
-        </td>
-        <td valign="top">
-            <textarea  name="details" maxlength="500" cols="99" rows="20"><?php echo $post_json ?></textarea>
-            </textarea>
-        </td>
-    </tr>
-
-    <tr>
-        <td valign="top">
-            <label for="details">Values Set</label>
-        </td>
-        <td valign="top">
-            <textarea  name="details" maxlength="500" cols="99" rows="20"><?php print_r($_POST);?></textarea>
-            </textarea>
-        </td>
-    </tr>
-
-    <tr>
-        <td valign="top">
-            <label for="details">RESPONSE</label>
-        </td>
-        <td valign="top">
-            <textarea  name="details" maxlength="500" cols="99" rows="20">
-                <?php
-
-                print_r($info);
-
-
-                print_r($runy);
-
-                ?>
-            </textarea>
-            </textarea>
-        </td>
-    </tr>
-
-</table>
-<br>
-<br>
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
 
 
