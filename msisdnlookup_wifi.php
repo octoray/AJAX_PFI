6a40fac6e4146cdcac3c3ddd5a5b5b5ca62adec2
D Wynter <david.wintour@gmail.com>
2017-03-29 09:58:06 +0100

    update paths

--- msisdnlookup_wifi.php
+++ msisdnlookup_wifi.php
@@ -1,8 +1,8 @@
 
 
-<script src="http://octoraypfi.co.uk/staging/jquery-2.1.4.min.js"></script>
+<script src="http://http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/jquery-2.1.4.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-<script src="http://octoraypfi.co.uk/staging/slicknav/dist/jquery.slicknav.js"></script>
+<script src="http://http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/slicknav/dist/jquery.slicknav.js"></script>
 
 
 <script type="text/javascript">
@@ -12,8 +12,8 @@
         dataType: "jsonp",
         data: {
             merchantToken: 'AE7A07A8-D085-40C6-ACE4-FF84F14E3247',
-            sessionToken: '70ACC6D7-7715-FA5B-BA98-B0FA35DA1C8F',
-            msisdn: '447584422934'
+            sessionToken: '4AA759DB-E08D-5BFB-2B02-DB0A5050B010',
+            msisdn: '447768240407'
         }
     }).done(function(result) {
             document.write(result);
