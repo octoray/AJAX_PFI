92d7b63f75402b5af0d1580e331b7424468a3e06
D Wynter <david.wintour@gmail.com>
2017-04-04 11:00:34 +0100

    Update landing.php

--- staging/landing.php
+++ staging/landing.php
@@ -10,8 +10,8 @@
     <!-- activate Menu -->
     <link rel="stylesheet" href="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/slicknav/dist/slicknav.css">
     <link rel="stylesheet" href="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/css/body.css" type="text/css">
-    <link rel="stylesheet" href="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/staging/css/fonts.css" type="text/css">
-    <script src="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/staging/jquery-2.1.4.min.js"></script>
+    <link rel="stylesheet" href="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/css/fonts.css" type="text/css">
+    <script src="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/jquery-2.1.4.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
     <script src="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/slicknav/dist/jquery.slicknav.js"></script>
 
