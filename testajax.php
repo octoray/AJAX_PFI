f5b857beaca9e14e6b4275a10086fa1ae48d18cc
D Wynter <david.wintour@gmail.com>
2017-06-15 10:15:00 +0100

    Create testajax.php

---
+++ testajax.php
@@ -0,0 +1,11 @@
+<div class="merchant-button btn2" id="hideme2" 
+data-pfi-button="true" 
+data-pfi-button-id="hidebutton2" 
+data-pfi-service-id="330" 
+data-pfi-reference="a2100fd1eecd158e70d2986e03538247ced714" 
+data-pfi-content-url="/staging/landing.php" 
+data-pfi-content-id="a2100fd1eecd158e70d2986e03538247ced714934924c875">
+   Buy ME
+</div>
+<br>
+<div class="block_666" data-pfi-area="true" id="showmeee" style=""> Content goes here </div>
