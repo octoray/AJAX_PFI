3985cb3957e8f97c875ef5c7a8fe52a2c796253f
D Wynter <david.wintour@gmail.com>
2017-09-19 11:35:45 +0100

    Update msisdnlookup_data.php

--- staging/msisdnlookup_data.php
+++ staging/msisdnlookup_data.php
@@ -1,27 +1,25 @@
-
-
 <script src="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/jquery-2.1.4.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 <script src="http://ec2-35-158-40-105.eu-central-1.compute.amazonaws.com/staging/slicknav/dist/jquery.slicknav.js"></script>
+
 <?php
 function getGUID(){
-if (function_exists('com_create_guid')){
-return com_create_guid();
-}else{
-mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-$charid = strtoupper(md5(uniqid(rand(), true)));
-$hyphen = chr(45);// "-"
-$uuid = // "{"
-substr($charid, 0, 8).$hyphen
-.substr($charid, 8, 4).$hyphen
-.substr($charid,12, 4).$hyphen
-.substr($charid,16, 4).$hyphen
-.substr($charid,20,12);
-return $uuid;
-}
+    if (function_exists('com_create_guid')){
+        return com_create_guid();
+    }else{
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = // "{"
+            substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12);
+        return $uuid;
+    }
 }
 ?>
-
 
 <script type="text/javascript">
     var start = new Date();
@@ -39,6 +37,3 @@ return $uuid;
             document.write('Error');
         });
 </script>
-
-
-
