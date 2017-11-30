24964b6da6c45273db9aab47cfce097654aea535
D Wynter <david.wintour@gmail.com>
2017-11-30 14:45:14 +0000

    Update test3.php

--- staging/test3.php
+++ staging/test3.php
@@ -4,18 +4,6 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <style>
-data-pfi-button {
-    cursor: pointer;
-    margin-left: 20%;
-    color: black;
-    font-size: 25px;
-    background: red;
-    margin-right: 20%;
-    text-align: center;
-    border-radius: 8px;
-}
-</style>
 
     <meta name="viewport" content="width=device-width">
     <meta charset="UTF-8">
@@ -31,7 +19,7 @@ data-pfi-button {
 <br>
 
     <!-- 2nd block /Purchase button -->
-    <div class="merchant-button btn2" id="hideme2" data-pfi-button="true" data-pfi-button-id="hidebutton2" data-pfi-service-id="330" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/landing.php" data-pfi-content-id="<?php echo $out2;?>">
+    <div class="merchant-button btn2" id="hideme2" data-pfi-button="true" data-pfi-button-id="hidebutton2" data-pfi-service-id="330" data-pfi-reference="<?php echo $out1;?>" data-pfi-content-url="/staging/landing.php" data-pfi-content-id="<?php echo $out2;?>" style="text-align: center;margin-left: 20%;;margin-right: 20%;">
         Buy
     </div>
     <br>
