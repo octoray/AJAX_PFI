5485cc847d18930bc7db8ac355390167284e4802
D Wynter <david.wintour@gmail.com>
2017-12-01 15:57:51 +0000

    Update test3.php
    add google analytics

--- staging/test3.php
+++ staging/test3.php
@@ -7,6 +7,15 @@
 
     <meta name="viewport" content="width=device-width">
     <meta charset="UTF-8">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-65637947-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-65637947-1');
+</script>
 
 </head>
 
