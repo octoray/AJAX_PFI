d808cca8d4db285ef0c28f1da3ab8163a71900a5
D Wynter <david.wintour@gmail.com>
2018-01-26 10:54:11 +0000

    Update inproc.php

--- inproc.php
+++ inproc.php
@@ -31,7 +31,7 @@ if (isset($_SERVER['HTTP_X_PFI_SESSIONTOKEN'])) {
 
 $response1 = '{
 	"Error":null,
-	"Script":"showDiscount()",
+	"Script":"nope()",
 	"InProcessUrl":null,
 	"ShowMarketingOptIn":true
 }';
