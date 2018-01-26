7e0bf3236457803b683173fe5b769473b597ea3a
D Wynter <david.wintour@gmail.com>
2018-01-26 10:58:41 +0000

    Update inproc.php

--- inproc.php
+++ inproc.php
@@ -40,7 +40,7 @@ $response2 = '{
    "error":null,
    "showMarketingOptIn":false,
    "inProcessUrl":null,
-   "script":"ewank.ImiHandler.handleStyling('')"
+   "script":"ewank.ImiHandler.handleStyling()"
 }';
 
 //respond
@@ -51,36 +51,4 @@ echo $response2;
 $data_json = json_decode($response2, true);
 
 
-?>
-<?php
-$username = "root";
-$password = "toor";
-$hostname = "localhost";
-$today1 = date("Y-m-d H:i:s");
-//connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password)
-or die("Unable to connect to MySQL");
-//echo "Connected to MySQL<br>";
-
-mysql_select_db("ajax") or die(mysql_error());
-$r1 = array();
-// chart1
-$result1 = mysql_query('INSERT INTO `ajax`.`inproc` (`HTTP_X_PFI_SESSIONTOKEN`, `HTTP_X_PFI_STATUS`, `HTTP_X_PFI_REQUESTTIME`, `HTTP_X_PFI_HASH`, `HTTP_X_PFI_ALIAS`, `HTTP_X_PFI_NETINFO`, `HTTP_X_PFI_CALLERID`, `HTTP_X_PFI_OPTINSTATUS`, `HTTP_X_PFI_TRANSACTIONID`, `Response`)
-VALUES
-("'.$_SERVER['HTTP_X_PFI_SESSIONTOKEN'].'",
- "'.$_SERVER['HTTP_X_PFI_STATUS'].'",
-  "'.$_SERVER['HTTP_X_PFI_REQUESTTIME'].'",
-   "'.$_SERVER['HTTP_X_PFI_HASH'].'",
-    "'.$_SERVER['HTTP_X_PFI_ALIAS'].'",
-     "'.$_SERVER['HTTP_X_PFI_NETINFO'].'",
-     "'.$_SERVER['HTTP_X_PFI_CALLERID'].'",
-     "'.$_SERVER['HTTP_X_PFI_OPTINSTATUS'].'",
-     "'.$_SERVER['HTTP_X_PFI_TRANSACTIONID'].'",
-      "Error: '.$data_json['Error'].'  Script: '.$data_json['Script'].'  InProcessUrl: '.$data_json['InProcessUrl'].'  ShowMarketingOptIn: '.$data_json['ShowMarketingOptIn'].'");')
-or die(mysql_error());
-
-
-
-
-mysql_close($dbhandle);
 ?>
